053a4788f4b4d90dbdb39f8ac594b0b953be1c1b
Wendy Harris <Wendy.Harris@ibm.com>
2022-01-12 15:40:55 -0500

    debugging

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,7 +6,7 @@ export default {
   devtool: "source-map",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./dist"),
     publicPath: "/",
     filename: "bundle.js",
   },
