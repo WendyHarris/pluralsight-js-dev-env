0c21e1fa543fad5b1f5ad3ad539c341fc910e6af
Wendy Harris <Wendy.Harris@ibm.com>
2022-01-14 11:51:29 -0500

    deploy

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/'; 
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://peaceful-coast-64059.herokuapp.com/'; 
 }
 
 function getQueryStringParameterByName(name, url) {
